1fd40cf3939ab1b50c9319fce20db180150d5e70
TechWiz007 <gajarebhargav@gmail.com>
2021-06-24 19:02:25 +0530

    sdm660-common: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+# Use These HAL's for 4.19
+git clone https://github.com/Xiaomi-Jasmine-ROMS/hardware_qcom-caf_sdm660_display.git -b sdm660-4.19 hardware/qcom-caf/sdm660/display
+git clone https://github.com/Xiaomi-Jasmine-ROMS/hardware_qcom-caf_sdm660_audio.git -b sdm660-4.19 hardware/qcom-caf/sdm660/audio
+git clone https://github.com/Xiaomi-Jasmine-ROMS/hardware_qcom-caf_sdm660_media.git -b sdm660-4.19 hardware/qcom-caf/sdm660/media
